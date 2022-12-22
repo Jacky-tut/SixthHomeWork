1f34a9e04bb54e5d10348869ea4210d518566c99
Jacky_tut <netfinnov@mail.ru>
2022-12-22 23:55:43 +0300

    1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,24 +1,18 @@
-﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-0, 7, 8, -2, -2 -> 2
-1, -7, 567, 89, 223-> 3
-*/
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
 
-int GetNumber(string message)
+
+
+Console.Write("Введите числа через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
 {
-    int result;
-    int count
-
-    while(true)
+    if (array[i] > 0)
     {
-        Console.WriteLine(message);
-        if(int.TryParse(Console.ReadLine(), out result))
-        {
-            break;
-        }
-        else
-        {
-            Console.WriteLine("Ввели не число");
-        }
+        count++;
     }
-    return result;
 }
+Console.WriteLine($"Кол-во элементов > 0 равно {count}");
