23a398893c64d91ff74b556373edec4b8f67b449
Jacky_tut <netfinnov@mail.ru>
2022-12-22 22:37:48 +0300

    2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,3 +2,23 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3
 */
+
+int GetNumber(string message)
+{
+    int result;
+    int count
+
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
